a8836ff36f9d3556aad18170d3fb2e61dff8d9cb
Jolin Shi <jolinshi_9@163.com>
2022-05-14 08:27:57 +0800

    前端新增代码

--- wj-vue/src/components/Login.vue
+++ wj-vue/src/components/Login.vue
@@ -1,13 +1,22 @@
 <template>
-  <div>
-    用户名:<input type="text" v-model="loginForm.username" placeholder="请输入用户名"/>
-    <br><br>
-    密码： <input type="password" v-model="loginForm.password" placeholder="请输入密码"/>
-    <br><br>
-    <button v-on:click="login">登录</button>
-  </div>
+  <body id = poster>
+  <el-form class="login-container" label-position="left"
+           label-width="0px">
+    <h3 class="login_title">Login</h3>
+    <el-form-item>
+      <el-input type="text" v-model="loginForm.username"
+                auto-complete="off" placeholder="username"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-input type="password" v-model="loginForm.password"
+                auto-complete="off" placeholder="password"></el-input>
+    </el-form-item>
+    <el-form-item style="width: 100%">
+      <el-button type="primary" style="width: 100%;background: #505458;border: none" v-on:click="login">login</el-button>
+    </el-form-item>
+  </el-form>
+  </body>
 </template>
-
 <script>
 
 export default {
@@ -35,6 +44,49 @@ export default {
         })
         .catch(failResponse => {
         })
+      this.axios({
+        method: 'post',
+        url: 'admin/getOpRecordByDate',
+        data: {
+          startTime: '2020-04-01 00:00:00',
+          endTime: '2020-04-03 00:00:00',
+          pageNo: 10,
+          pageSize: 10}
+      }).then((res) => {
+        this.tableData = res.data.list
+      })
     }
   }}
 </script>
+<style>
+  .login-container {
+    border-radius: 15px;
+    background-clip: padding-box;
+    margin: 90px auto;
+    width: 350px;
+    padding: 35px 35px 15px 35px;
+    background: #fff;
+    border: 1px solid #eaeaea;
+    box-shadow: 0 0 25px #cac6c6;
+  }
+
+  .login_title {
+    margin: 0px auto 40px auto;
+    text-align: center;
+    color: #505458;
+  }
+  #poster {
+    background:url("../assets/login.jpg") no-repeat;
+    background-position: center;
+    height: 100%;
+    width: 100%;
+    background-size: cover;
+    position: fixed;
+    vertical-align: middle;
+    display: block;
+    vertical-align: bottom;
+  }
+  body{
+    margin: 0px;
+  }
+</style>
---
+++ wj-vue/src/components/common/NavMenu.vue
@@ -0,0 +1,50 @@
+<template>
+  <el-menu
+    :default-active="'/index'"
+    router
+    mode="horizontal"
+    background-color="white"
+    text-color="#222"
+    active-text-color="red"
+    style="min-width: 1300px">
+    <el-menu-item v-for="(item,i) in navList" :key="i" :index="item.name">
+      {{ item.navItem }}
+    </el-menu-item>
+    <span style="position: absolute;padding-top: 20px;right: 60%;font-size: 20px;font-weight: bold">yayanovel</span>
+    <el-input
+      placeholder="快速搜索..."
+      prefix-icon="el-icon-search"
+      size="medium"
+      style="width: 300px;position:absolute;margin-top: 12px;right: 18%"
+      v-model="keywords">
+    </el-input>
+  </el-menu>
+
+</template>
+
+<script>
+export default {
+  name: 'NavMenu',
+  data () {
+    return {
+      navList: [
+        {name: '/index', navItem: 'Home'},
+        {name: '/jotter', navItem: 'Jotter'},
+        {name: '/book', navItem: 'Book'},
+        {name: '/admin', navItem: '个人中心'}
+      ],
+      keywords: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+  a{
+    text-decoration: none;
+  }
+
+  span {
+    pointer-events: none;
+  }
+</style>
